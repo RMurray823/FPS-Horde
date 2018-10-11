14dea464c7341d9a568263dd0c116d840ee105ce
MMaluschka13 <MMaluschka13@users.noreply.github.com>
2018-10-11 10:28:19 -0500

    Added enemy respawn prefab.

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    //public playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints; //used to hold multiple locations for enemies to spawn from.
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime); //causes this script to run once for each interval spawnTime.
+	}
+
+    void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+	
+	/* Update is called once per frame (unneeded in this implementation)
+	void Update ()
+    {
+		
+	}*/
+}
