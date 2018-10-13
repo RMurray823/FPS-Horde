e7ceb30807acfd265ad1e30a911f81511c5cc733
MMaluschka13 <MMaluschka13@users.noreply.github.com>
2018-10-13 09:45:32 -0500

    Added enemy damage to player including animation.

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -5,27 +5,38 @@ using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    private NavMeshAgent _nav;
-    private Transform _player;
+    private NavMeshAgent nav;
+    private Transform player;
     private Health health;
-    private Animator _anim;
+    private Animator anim;
+    private int damage = 5;
+
 	// Use this for initialization
 	void Start ()
     {
-        _nav = GetComponent<NavMeshAgent>(); //get NavMesh component.
-        _player = GameObject.FindGameObjectWithTag("Player").transform; //find a player.
+        nav = GetComponent<NavMeshAgent>(); //get NavMesh component.
+        player = GameObject.FindGameObjectWithTag("Player").transform; //find a player.
         health = GetComponent<Health>();
-        _anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
+        //check to update the animation method every half second.
+        InvokeRepeating("Animation", 0.5f, 0.5f);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        _nav.SetDestination(_player.position); //move to player's position.
-        if (_anim.velocity.magnitude != 0)
-            _anim.SetBool("isMoving", true);
-        else
-            _anim.SetBool("isMoving", false);
+        nav.SetDestination(player.position); //move to player's position.
+
 	}
+
+    void Animation()
+    {
+        if (Vector3.Distance(this.transform.position, player.transform.position) > nav.stoppingDistance)
+            anim.SetBool("isMoving", true);
+        else
+            anim.SetTrigger("attack");
+        player.BroadcastMessage("hit", damage);
+            anim.SetBool("isMoving", false);
+    }
 
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "HealthPack") {
