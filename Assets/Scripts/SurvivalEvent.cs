88139c4826f5e0ba9cc249a62cb6500228a8a9c0
MMaluschka13 <MMaluschka13@users.noreply.github.com>
2018-10-17 20:35:35 -0500

    Fixed Zombie animations, again, and reorganized meshes, etc, into sprites folder.

---
+++ Assets/Scripts/SurvivalEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurvivalEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEvent(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            Debug.Log("FPlayer within area.");
+    }
+}
