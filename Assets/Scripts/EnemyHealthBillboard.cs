c1de82df1131e10419e18bf19dbf2fafe95cd378
maelstromdrift <mgitthens@gmail.com>
2018-11-09 14:30:10 -0600

    Removed function stub.

--- Assets/Scripts/EnemyHealthBillboard.cs
+++ Assets/Scripts/EnemyHealthBillboard.cs
@@ -3,13 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHealthBillboard : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
         transform.LookAt(Camera.main.transform);
 	}
