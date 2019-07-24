b23c54d20cfe941b2ea75816ca50febe18425776
Marc Sustaita <marc.sustaita@gmail.com>
2019-07-24 18:02:02 -0500

    Camera Changes???

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     private float rotationY = 0f;
+    private float rotationX = 0f;
     private Vector3 mouseInput = Vector3.zero;
+
+    public bool gunFiring = false;
 
     // Update is called once per frame
     void Update() {
-        HandleInput();
+
+            HandleInput();
+      
     }
 
     private void HandleInput() {
@@ -18,12 +23,32 @@ public class CameraController : MonoBehaviour {
 
         mouseInput = Vector3.zero;
         mouseInput.y = -Input.GetAxis("Mouse Y");
+        mouseInput.x = Input.GetAxis("Mouse X");
 
         rotationY += mouseInput.y * 100f * Time.deltaTime;
+        rotationX += mouseInput.x * 100f * Time.deltaTime;
 
-		rotationY = Mathf.Clamp (rotationY, -90, 90);
-        Quaternion localRotation = Quaternion.Euler(rotationY, 0, 0);
+        rotationY = Mathf.Clamp (rotationY, -90, 90);
+
+        if (gunFiring == true) {
+            rotationY -= 2 ;
+            gunFiring = false;
+        }
+
+        Quaternion localRotation = Quaternion.Euler(rotationY, rotationX, 0);
 
         transform.localRotation = localRotation;
     }
+
+    public void Firing(bool currentlyFiring) {
+        if (currentlyFiring == true)
+        {
+            gunFiring = true;
+        }
+    }
+       
+
+
+
+
 }
